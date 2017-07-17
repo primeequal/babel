e56e20faf3779c3a5616b00d0ed9fbc90405a970
Victor Ude <victor@Victors-MacBook-Pro.local>
2017-07-17 16:49:57 -0500

    adding ignore file and stop script

--- start_platform.sh
+++ start_platform.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 TAG=master-0
+HOST_PORT=4000
+CONTAINER_PORT=80
 
+bash /vagrant/stop_platform.sh
+
+COUNTER=0
 gcloud container images list | tail -n +2 - | \
- while read x; do echo "gcloud docker -- pull $x:$TAG" && gcloud docker -- pull $x:$TAG; done
+ while read x; do
+     echo "Running docker image $x:$TAG" && gcloud docker -- run -d -p $(($HOST_PORT+$COUNTER)):$CONTAINER_PORT $x:$TAG
+     COUNTER=$COUNTER+1
+ done
